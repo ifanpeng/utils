7267d3160853e684e651c3b9260539454bc5a5d0
jieke17weiju <jieke@17weiju.com>
2022-02-15 10:51:44 +0800

    feat: 添加更新时间显示

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,21 +4,36 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: '/images/favicon.png' }]
     ],
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+                transformer: (timestamp, lang) => {
+                    // 不要忘了安装 moment
+                    const moment = require('moment')
+                    moment.locale(lang)
+                    return moment(timestamp).format('YYYY-MM-DD')
+                }
+            }
+        ]
+    ],
     themeConfig: {
+        lastUpdated: '更新时间',
         logo: '/images/logo.png',
         nav: [
             { text: 'Home', link: '/' },
             { text: '接口管理平台', link: 'https://www.bbccdd.cn/' },
-            { 
-                text: '其他', 
-                items:[
-                    { 
+            { text: '网址导航', link: 'https://isoso.co/' },
+            {
+                text: '其他',
+                items: [
+                    {
                         text: '添加QQ好友',
                         link: 'tencent://Message/?Uin=1763535096'
                     }
                 ]
             },
-          ],
+        ],
         sidebar: [
             'src/common/introduce',
             {
@@ -33,5 +48,5 @@ module.exports = {
                 ]
             }
         ]
-      },
+    },
 }
